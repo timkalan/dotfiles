6326274d99004ed028db083801bd0f72d00f22e9
Tim Kalan <timkalan99@gmail.com>
2024-09-17 13:47:57 +0200

    mac keyboard stuff

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,4 +9,11 @@ config.font_size = 12.0
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
+-- TEMP fix for perf issue
+config.window_background_opacity = 0.999
+
+-- config.disable_default_key_bindings = true
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
 return config
