0f08dd00768da51fe6f68580efa7e76025c1a012
Tim Kalan <timkalan99@gmail.com>
2024-10-15 19:26:37 +0200

    change vim viewer: sioyek -> skim

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,6 @@ return {
 	lazy = false, -- we don't want to lazy load VimTeX
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
-		vim.g.vimtex_view_method = "sioyek"
+		vim.g.vimtex_view_method = "skim"
 	end,
 }
