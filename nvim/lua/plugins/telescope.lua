8c34ee67e3fce70bb7323d75214f194bd32a8bf0
Tim Kalan <timkalan99@gmail.com>
2024-10-22 18:29:34 +0200

    fzf telescope working

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,45 +1,46 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"debugloop/telescope-undo.nvim",
-			"smartpde/telescope-recent-files",
-		},
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						hidden = true,
-					},
-				},
-			})
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = "make"
+            },
+            { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+            "nvim-lua/plenary.nvim",
+            "debugloop/telescope-undo.nvim",
+            "smartpde/telescope-recent-files",
+            "nvim-telescope/telescope-ui-select.nvim",
+        },
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                    },
+                },
+            })
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-			require("telescope").load_extension("undo")
-			vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>")
+            require("telescope").load_extension("undo")
+            vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>")
 
-			require("telescope").load_extension("recent_files")
-			vim.keymap.set("n", "<leader>fr", "<cmd>lua require('telescope').extensions.recent_files.pick()<CR>")
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            require("telescope").load_extension("recent_files")
+            vim.keymap.set("n", "<leader>fr", "<cmd>lua require('telescope').extensions.recent_files.pick()<CR>")
+
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("ui-select")
+        end,
+    }
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -8,6 +8,12 @@ vim.cmd("set shiftwidth=4")
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- column line
+-- vim.opt.colorcolumn = "120"
+
+-- save undo history
+vim.opt.undofile = true
+
 -- indentation
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -18,13 +24,17 @@ vim.o.clipboard = "unnamedplus"
 -- persistent gutter
 vim.opt.signcolumn = "yes"
 
--- leader
-vim.g.mapleader = " "
-
 -- smarter searching
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.hlsearch = false
+
+-- opening splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- leader
+vim.g.mapleader = " "
 
 -- centering
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -33,9 +43,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
-
--- save undo history
-vim.opt.undofile = true
 
 -- quickfix bindings
 vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>")
