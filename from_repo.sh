d0ba7ef4ebe476d471337eedf73ac8d490d7882a
Tim Kalan <timkalan@diego.fritz.box>
2024-10-22 18:48:22 +0200

    restructure repo: move to home folder

--- from_repo.sh
+++
@@ -1,8 +0,0 @@
-# copy repo version to local
-
-cp $PWD/.wezterm.lua ~
-cp $PWD/.zshrc ~
-cp $PWD/.tmux.conf ~
-cp -r $PWD/nvim ~/.config
-
-echo success
--- to_repo.sh
+++
@@ -1,8 +0,0 @@
-# copy local dotfiles to repo
-
-cp ~/.wezterm.lua $PWD
-cp ~/.zshrc $PWD
-cp ~/.tmux.conf $PWD
-cp -r ~/.config/nvim $PWD
-
-echo success
